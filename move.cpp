1be6f8f2b55a9c5a7d99dc2344544225937c90f3
Seán Rourke <seanrourke3@gmail.com>
2025-02-04 09:44:52 +0000

    added docygen comments for move.h and move.cpp

--- move.cpp
+++ move.cpp
@@ -1,5 +1,14 @@
 #include "move.h"
 
+/**
+ * @brief Overloads the << operator to print a move.
+ * 
+ * This function formats a Move object as (from, to) and outputs it to a stream.
+ * 
+ * @param os The output stream.
+ * @param move The Move object to be printed.
+ * @return std::ostream& The modified output stream.
+ */
 std::ostream &operator<<(std::ostream &os, const Move &move) {
     os << "(" << move.from << ", " << move.to << ")";
     return os;
--- move.h
+++ move.h
@@ -3,11 +3,24 @@
 
 #include <iostream>
 
+/**
+ * @struct Move 
+ * @brief Represents a chess move.
+ * 
+ * This structure stores the starting and ending squares of a move.
+ * It also includes an optional promotion piece filed for pawn promotions.
+ */
 struct Move {
-    int from;
-    int to;
-    int promotionPiece = -1; // Optional, default to -1 if no promotion
-
+    int from;   ///< The starting square of the move (0-63).
+    int to;     ///< The ending square of the move (0-63).
+    int promotionPiece = -1;    ///< The promoted piece type (if applicable, otherwise -1).
+    
+    /**
+     * @brief Overloads the << operator to print a move.
+     * @param os The output stream.
+     * @param move The move to be printed.
+     * @return std::ostream& The output stream with the move formatted.
+     */
     friend std::ostream &operator<<(std::ostream &os, const Move &move);
 };
 
