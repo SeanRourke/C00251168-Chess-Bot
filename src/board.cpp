3aa6272a384fffb3f4d0ddf790b85671114d501f
Seán Rourke <seanrourke3@gmail.com>
2025-02-13 11:45:33 +0000

    materialCount test with a random position

--- src/board.cpp
+++ src/board.cpp
@@ -29,23 +29,24 @@ Board::Board() { initialise(); }
  */
 void Board::initialise()
 {
-    bitboards[PAWN][WHITE] = 0x000000000000FF00;
-    bitboards[PAWN][BLACK] = 0x00FF000000000000;
+    bitboards[PAWN][WHITE]  = 0x0000000000083200;  // Pawns on d2, f3, g3, e5
+    bitboards[PAWN][BLACK]  = 0x0000200000008800;  // Pawns on e6, d7, h7
 
-    bitboards[ROOK][WHITE] = 0x0000000000000081;
-    bitboards[ROOK][BLACK] = 0x8100000000000000;
+    bitboards[ROOK][WHITE]  = 0x0000000000000080;  // Rook on h1
+    bitboards[ROOK][BLACK]  = 0x0000000000000000;  // No black rooks
 
-    bitboards[KNIGHT][WHITE] = 0x0000000000000042;
-    bitboards[KNIGHT][BLACK] = 0x4200000000000000;
+    bitboards[KNIGHT][WHITE] = 0x0000000000000004; // Knight on c1
+    bitboards[KNIGHT][BLACK] = 0x0000000000000000; // No black knights
 
-    bitboards[BISHOP][WHITE] = 0x0000000000000024;
-    bitboards[BISHOP][BLACK] = 0x2400000000000000;
+    bitboards[BISHOP][WHITE] = 0x0000000000000400; // Bishop on e3
+    bitboards[BISHOP][BLACK] = 0x0000000000002000; // Bishop on g6
 
-    bitboards[QUEEN][WHITE] = 0x0000000000000008;
-    bitboards[QUEEN][BLACK] = 0x0800000000000000;
+    bitboards[QUEEN][WHITE]  = 0x0000000000000008; // Queen on d1
+    bitboards[QUEEN][BLACK]  = 0x0000000800000000; // Queen on d6
 
-    bitboards[KING][WHITE] = 0x0000000000000010;
-    bitboards[KING][BLACK] = 0x1000000000000000;
+    bitboards[KING][WHITE]   = 0x0000000000000010; // King on e1
+    bitboards[KING][BLACK]   = 0x1000000000000000; // King on e8
+
 
 
     pieces.fill(EMPTY);
